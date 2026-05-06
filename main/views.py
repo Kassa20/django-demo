38e0ab14735f508fcbec383be33a32af4359c4d1
neo <ykassa47@gmail.com>
2026-05-05 19:54:50 -0700

    initial django app

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from main.models import Message
+
+
+def home(request):
+    messages = Message.objects.all()
+    output = "Messages:<br>"
+    
+    for msg in messages:
+        output += msg.text + "<br>"
+    
+    return HttpResponse(output)
